d5fc54f4a401af595efba06098a9fc12c2d44d5e
shareNOU <75669928+shareNOU@users.noreply.github.com>
2021-01-18 19:20:58 +0900

    ユーザー管理機能単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
   validates :nickname,         presence: true
-  validates :first_name,       presence: true
-  validates :family_name,      presence: true
-  validates :first_name_kana,  presence: true
-  validates :family_name_kana, presence: true
+  validates :first_name,       presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name,      presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_kana,  presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :family_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_date,       presence: true
 
 end
