a9f551c88f468e680781c30f7020b32fa85f71b5
shareNOU <75669928+shareNOU@users.noreply.github.com>
2021-01-25 16:41:56 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,8 +27,8 @@ class ItemsController < ApplicationController
 
   def update
     @item.update(item_params)
-    if @item.save
-      redirect_to item_path
+    if @item.valid?
+      redirect_to item_path(@item.id)
     else
       render :edit
     end
