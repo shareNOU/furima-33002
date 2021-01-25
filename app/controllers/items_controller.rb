04eeaf586c9b944681e007c15a31271f5b84faae
shareNOU <75669928+shareNOU@users.noreply.github.com>
2021-01-25 17:52:48 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,7 +36,11 @@ class ItemsController < ApplicationController
 
   def destroy
     @item.destroy
-    redirect_to root_path
+    if @item.valid?
+      redirect_to root_path
+    else
+      render :show
+    end
   end
 
   private
