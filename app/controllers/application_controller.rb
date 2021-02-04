a982b3da38d9d185417d2064626b9a8e67046807
shareNOU <75669928+shareNOU@users.noreply.github.com>
2021-02-04 19:33:57 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
-
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_date])
+  end
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'furifrui' && password == '1234'
+    end
   end
 end
