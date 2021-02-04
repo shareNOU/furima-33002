68360473f4589e5e12ccb6fc68c71dd4b4ec142a
shareNOU <75669928+shareNOU@users.noreply.github.com>
2021-02-04 19:40:02 +0900

    syuusei

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   end
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'furifrui' && password == '1234'
+      username == 'furifuri' && password == '1234'
     end
   end
 end
